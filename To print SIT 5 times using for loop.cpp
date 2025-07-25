40be8e4e6d247213c53915f1fdaecf90b2b160c4
65NinadA3 <ninadphatak123@gmail.com>
2025-07-25 22:35:46 +0530

    Create To print SIT 5 times using for loop.cpp

---
+++ To print SIT 5 times using for loop.cpp
@@ -0,0 +1,12 @@
+// Name : Ninad Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0;i<=5;i++) 
+    {cout<<"SIT"<<endl;}
+    
+    return 0;
+}
