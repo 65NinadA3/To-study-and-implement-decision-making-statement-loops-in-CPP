c7e4b5324ab91ac9f8282288ae1490958e98ba3a
65NinadA3 <ninadphatak123@gmail.com>
2025-07-25 22:33:20 +0530

    Create create 1 to 20 by for loop.cpp

---
+++ create 1 to 20 by for loop.cpp
@@ -0,0 +1,10 @@
+//Name : Niand Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<20){cout<<i+1<<" ";i++;cout<<endl;}
+    return 0;
+}
