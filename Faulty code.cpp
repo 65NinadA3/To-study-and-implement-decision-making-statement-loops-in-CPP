d2e08185b74e9d56153dd56071f13cd076a223d2
65NinadA3 <ninadphatak123@gmail.com>
2025-07-26 12:46:50 +0530

    Update Faulty code.cpp

--- Faulty code.cpp
+++ Faulty code.cpp
@@ -2,19 +2,14 @@
 //PRN : 24070123065
 //Batch: ENTC A3
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int i, j;
     int n = 5;
-
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             cout << "* ";
-        
-        cout << endl;
+            cout << endl;
     }
-
     return 0;
 }
